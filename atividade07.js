884a9c7f57a60e90d712f5e6f85cef9bf8697c8d
Ana Luiza de Freitas <132159788+analufreitasx@users.noreply.github.com>
2023-07-09 19:01:12 -0300

    Add files via upload

---
+++ atividade07.js
@@ -0,0 +1,5 @@
+const anoatual = 2023;
+const nome = prompt (' Bem vindo! Digite seu nome: ');
+const ano_nascimento = prompt (' Agora, digite seu ano de nascimento: ');
+let idade = anoatual - ano_nascimento;
+alert('Olá, o seu nome é ' + nome + ' e você tem ' + idade + ' anos.');
